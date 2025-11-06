17ef33148f9a3b3eb74a1065d0ddd88a2c157d4d
derboerner08 <karlboerner2008@gmail.com>
2025-11-06 11:44:17 +0100

    Add Menge class for set operations
    Implement Menge class with basic set operations

---
+++ menge.py
@@ -0,0 +1,64 @@
+class Menge:
+    def __init__(self):
+        self.menge = []
+        
+    def show(self):
+        print("{", end="")
+        for el in self.menge[:-1]:
+            print(el, end=", ")
+        if len(self.menge)>0:
+            print(self.menge[-1], end="")
+        print("}")
+        
+    def isEmpty(self):
+        return self.menge==[]
+    
+    def insert(self,el):
+        if el not in self.menge:
+            self.menge += [el]
+    
+    def remove(self,el):
+        if el in self.menge:
+            self.menge.remove(el)
+            
+    def part(self, el):
+        if el in self.menge:
+            return True
+        else:
+            return False
+        
+    def lang(self):
+        return len(self.menge)
+    
+    def vereinigen(self,partner):
+        neu=Menge()
+        for i in range(len(partner.menge)):
+            A.insert(partner.menge[i])
+        for a in range(len(partner.menge)):
+            A.insert(self.menge[i])
+        return A
+            
+    def schnitt(self,partner):
+        A = Menge()
+        for i in range (len(self.menge)):
+            if self.menge[i] in partner.menge:
+                A.insert(self.menge[i])
+        return A
+    
+    
+            
+    
+    
+    
+    
+    
+    
+
+        
+A = Menge()
+A.insert(6)
+A.insert(7)
+A.show()
+A.remove(6)
+A.show()
+print(A.lang())
